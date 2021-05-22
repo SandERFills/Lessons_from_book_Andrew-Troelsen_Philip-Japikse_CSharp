b8c0fdaf9e8514ad35108760f43d3f412da77a05
SandERFills <salagaev98@mail.ru>
2021-05-22 16:58:06 +0300

    beginning overload

---
+++ MethodsOverLoading/MethodsOverLoading/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsOverLoading
+{
+    class Program
+    {
+        static int Add(int a, int b) {
+            
+            return a + b; 
+        }
+        static double Add(double a, double b)
+        {
+
+            return a + b;
+        }
+        static long Add(long a, long b)
+        {
+
+            return a + b;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Add(100_1111_111,111_222_333_444));
+            Console.ReadLine();
+        }
+    }
+}
