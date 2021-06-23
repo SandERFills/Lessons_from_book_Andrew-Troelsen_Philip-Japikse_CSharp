9d37afa31b20367eece462713f87e2828ce406b6
SandERFills <salagaev98@mail.ru>
2021-06-23 22:24:25 +0300

    Passed lesson about parent class Object

---
+++ ObjectOverride/Person.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+namespace ObjectOverride
+{
+	public class Person : Object
+	{
+		public Person()
+		{
+			//
+			// TODO: Add constructor logic here
+			//
+		}
+        public int Age { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+		public Person(string name,string lastname,int age) {
+			Age = age;
+			Name = name;
+			LastName = lastname;
+		
+		}
+    }
+}
+
---
+++ ObjectOverride/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ObjectOverride
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Fun with System.Object *****\n");
+            Person program=new Person(name:"Posos",lastname:"Stricos",12);
+            Console.WriteLine("{0}",program.GetHashCode());
+            Console.WriteLine("{0} ",program.GetType());
+            Console.WriteLine("{0} ",program.ToString());
+            Person person=program;
+            Object person2=person;
+            if (person.Equals(person2) && program.Equals(person))
+	{
+                Console.WriteLine("insance");
+	}
+            Console.ReadLine();
+        }
+    }
+}
