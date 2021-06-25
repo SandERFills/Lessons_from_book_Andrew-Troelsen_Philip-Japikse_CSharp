d6a9b7a24f9cd5e5a2fe7ee31a804dc8f58811af
Babich <babich.vr@talantiuspeh.ru>
2021-06-25 18:45:17 +0300

    Special Excaption Application level first method

---
+++ CustomException/CarIsDeadException.cs
@@ -0,0 +1,29 @@
+using System;
+// using System.Exception;
+// using System.ApplicationException;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomExceptoin
+{
+    class carIsDeadException : ApplicationException
+    {
+        private string messageDetails=String.Empty;
+        public DateTime ErrorTimeStamp{get;set;}
+public string CauseOfError{get;set;}
+public carIsDeadException(){}
+public carIsDeadException(string message,string cause,DateTime time){
+    messageDetails=message;
+    CauseOfError=cause;
+    ErrorTimeStamp=time;
+    }
+    public override string Message=>$"Car Error Message: {messageDetails}";
+    }
+
+
+
+
+}
