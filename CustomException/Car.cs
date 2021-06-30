f46873f818dc2a38ddd26e6b4a88b01378001cb0
Babich <babich.vr@talantiuspeh.ru>
2021-06-30 09:23:09 +0300

    Work version exception topic

--- CustomException/Car.cs
+++ CustomException/Car.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CustomExceptoin
+namespace ProcessMultipleExpception
 {
     class Car
     {
--- CustomException/CarIsDeadException.cs
+++ CustomException/CarIsDeadException.cs
@@ -7,15 +7,30 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CustomExceptoin
+namespace ProcessMultipleExpception
 {
+    [Serializable]
     class carIsDeadException : ApplicationException
     {
         private string messageDetails=String.Empty;
         public DateTime ErrorTimeStamp{get;set;}
 public string CauseOfError{get;set;}
+//Третий способ основывается на следующих характеристиках: 
+// • является производным от класса Exception/ApplicationException; 
+// • помечен атрибутом [System.Serializable]: 
+// • определяет стандартный конструктор: 
+// • определяет конструктор, который устанавливает значение унаследованного свойства Message: 
+// • определяет конструктор для обработки “внутренних исключений”: 
+// • определяет конструктор для поддержки сериализации типа.
 public carIsDeadException(){}
+public carIsDeadException(string message):base(message){}
+public carIsDeadException(string message,System.Exception inner):base(message,inner){}
+protected carIsDeadException(
+    System.Runtime.Serialization.SerializationInfo info,
+    System.Runtime.Serialization.StreamingContext context
+):base(info,context){}
 public carIsDeadException(string message,string cause,DateTime time):base(message){
+    //Второй способ заключается в передаче сообщения базовому классу
     // messageDetails=message;
     CauseOfError=cause;
     ErrorTimeStamp=time;
