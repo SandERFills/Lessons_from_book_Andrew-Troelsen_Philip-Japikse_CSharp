1321f930678c4bdc8123f2eb0281ded3f581c2b7
SandERFills <salagaev98@mail.ru>
2021-07-05 16:15:13 +0300

    Lesson about interface

---
+++ CostomInterface/Cyrcle.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CostomInterface
+{
+    class Cyrcle:Shape,IDraw
+    {
+        public Cyrcle(){}
+        public Cyrcle(string name) : base(name)
+        {
+
+        }
+        public void Draw3D()
+        {
+
+        }
+        public override void Draw(){
+            Console.WriteLine("Drawing {0} the Cyrcle",PetName);
+        }
+    }
+}
---
+++ CostomInterface/Triangle.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CostomInterface
+{
+    class Triangle : Shape, IPointy
+    {
+        public byte Points { get { return 3; } }
+
+        public override void Draw()
+        {
+            Console.WriteLine("Draw triangle {0}",PetName);
+        }
+        public Triangle(string name) : base(name)
+        {
+
+        }
+
+    }
+}
