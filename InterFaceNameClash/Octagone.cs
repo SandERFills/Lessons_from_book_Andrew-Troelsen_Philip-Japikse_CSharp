7c8955b0b074d29846698a70ec7cb4f2195f88a6
SandERFills <salagaev98@mail.ru>
2021-07-05 19:00:26 +0300

    Styded interface using case and type conversion

---
+++ InterFaceNameClash/Octagone.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterFaceNameClash
+{
+    class Octagone : IDrawToForm, IDrawToMemory, IDrawToPrinter
+    {
+      void   IDrawToForm.Draw()
+        {
+            Console.WriteLine("I draw to form");
+        }
+         void IDrawToMemory.Draw()
+        {
+            Console.WriteLine("I draw to memory");
+        }
+         void IDrawToPrinter.Draw()
+        {
+            Console.WriteLine("I draw to printer");
+        }
+    }
+}
---
+++ InterFaceNameClash/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace InterFaceNameClash
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Octagone oct = new Octagone();
+            //Direct conversion with interface variable
+            IDrawToPrinter drawToPrinter = (IDrawToPrinter)oct;
+            drawToPrinter.Draw();
+            //Short version without interface variable
+            ((IDrawToForm)oct).Draw();
+            //With "IS" and assigning Interface variable
+            if (oct is IDrawToMemory dtm)
+            {
+                dtm.Draw();
+            }
+            Console.ReadLine();
+        }
+    }
+}
