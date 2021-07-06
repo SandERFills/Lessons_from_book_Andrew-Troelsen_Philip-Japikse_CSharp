1ca3423a1995c89d946d1a637aab4577c71132ef
SandERFills <salagaev98@mail.ru>
2021-07-06 17:59:32 +0300

    Lesson about expand interface

---
+++ InterfaceHierarchy/IAdvancedDraw.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceHierarchy
+{
+    interface IAdvancedDraw : IDrawable//We epand interface
+    {
+        void DrawInBoundingBox(int left, int top, int right, int botton);
+        void DrawUpSideDown();
+    }
+}
---
+++ InterfaceHierarchy/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace InterfaceHierarchy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bitmaps bitmaps = new Bitmaps();
+            bitmaps.Draw();
+            bitmaps.DrawInBoundingBox(1,2,3,4);
+            bitmaps.DrawUpSideDown();
+            IAdvancedDraw advancedDraw = bitmaps as IAdvancedDraw;
+            advancedDraw.DrawUpSideDown();
+            Console.ReadLine();
+        }
+    }
+}
