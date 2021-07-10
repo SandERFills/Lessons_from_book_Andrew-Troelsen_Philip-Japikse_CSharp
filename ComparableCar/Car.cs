44324038b4e9f87fee2f5821256e0a08bb51c4b7
SandERFills <salagaev98@mail.ru>
2021-07-10 13:36:44 +0300

    Begging  ICompare interface lesson

---
+++ ComparableCar/Car.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComparableCar
+{
+    class Car:IComparable
+    {
+        public Car()
+        {
+
+        }
+        public Car(string st,int curspeed,int id)
+        {
+            CurrSpeed = curspeed;
+            CarID = id;
+            PetName = st;
+        }
+        public string PetName { get; set; }
+        public int CurrSpeed { get; set; }
+        public int CarID { get; set; }
+
+        public int CompareTo(object obj)//Реализация метода интерфейса IComparable
+        {
+            Car temp = obj as Car;
+            if (temp !=null)
+            {
+                if (this.CarID>temp.CarID)
+                {
+                    return 1;
+                }
+                if (this.CarID<temp.CarID)
+                {
+                    return -1;
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+            else
+            {
+                throw new ArgumentException("Parametr is not a Car");
+            }
+        }
+
+        public void PrintSpeed() => Console.WriteLine("{0} is going {1} km/h",PetName,CurrSpeed);
+        public void SpeedUp(int delta) => CurrSpeed += delta;
+    }
+}
---
+++ ComparableCar/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ComparableCar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        Console.WriteLine("***** Fun with Object Sorting!");
+            Car[] cars = new Car[5];
+            cars[0] = new Car("Rusty", 80, 1) ;
+            cars[1] = new Car("Mary", 40, 234);
+            cars[2] = new Car("Viper", 40, 34);
+            cars[3] = new Car("Mel", 40, 4);
+            cars[4] = new Car("Chucky", 40, 5);
+
+
+        }
+    }
+}
