0f1af740c8f5387759bf77a8ce0d98a0cce60cfd
Vladislav Babich <salagaev98@mail.ru>
2021-07-21 19:56:59 +0300

    Topic <>

---
+++ CustomGenericMethods/PersonCollection.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.ObjectModel;
+
+
+namespace CustomGenericMethods 
+{
+   public class Person{
+       public int Age{get;set;}
+       public string FirstName{get;set;}
+       public string SecondName{get;set;}
+       public Person(){}
+       public Person(string firstname,string secondname ,int age){
+           Age=age;
+           FirstName=firstname;
+           SecondName=secondname;
+       }
+       public override string ToString(){
+           return $"Name: {FirstName} {SecondName}, Age= {Age}";
+       }
+   }
+   public class PersonCollection: IEnumerable
+    {
+        private ArrayList arPeople=new ArrayList();
+        //приведение для вызывающего кода
+        public Person GetPerson(int pos)=>(Person)arPeople[pos];
+         //вставка только объектов Person
+         public void AddPerson(Person p){
+             arPeople.Add(p);
+         }
+         public void ClearPeople(){
+             arPeople.Clear();
+         }
+         public int Count=>arPeople.Count;
+         //ПОддержка перечисления с помощью foreach
+         IEnumerator IEnumerable.GetEnumerator()=>arPeople.GetEnumerator();
+    }
+}
---
+++ CustomGenericMethods/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.ObjectModel;
+
+namespace CustomGenericMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a=10,b=90;
+            Console.WriteLine("Before swap: {0},{1}",a,b);
+            Swap<int>(ref a,ref b);
+            System.Console.WriteLine("After swap: {0},{1}",a,b);
+            string first ="Hello",second="second";
+            Swap<string>(ref first,ref second);
+            System.Console.WriteLine();
+            bool b1=true,b2=false;
+            Console.WriteLine("Before swap: {0},{1}",b1,b2);
+            Swap(ref b1,ref b2);//Обобщённые методы могу автоматически определять тип по параметру
+            System.Console.WriteLine("After swap: {0},{1}",b1,b2);
+        }
+        static void Swap(ref int a,ref int b){
+            int temp=a;
+            a=b;
+            b=temp;
+        }
+        static void Swap(ref Person a ,ref Person b){
+            Person temp=a;
+            a=b;
+            b=temp;
+        }
+        static void Swap<T>(ref T a,ref T b){
+            System.Console.WriteLine("You sent the Swap() method a {0}",typeof(T));
+            T temp=a;
+            a=b;
+            b=temp;
+        }
+        static void DisplayBaseClass<T>(){
+            System.Console.WriteLine("Base class of {0} is: {1} ",typeof(T),typeof(T).BaseType);
+        }
+    }
+}
