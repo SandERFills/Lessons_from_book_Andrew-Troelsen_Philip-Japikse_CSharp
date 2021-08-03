86b7eec020c9aa4ac8f701997d15eff33a702abb
SandERFills <salagaev98@mail.ru>
2021-08-03 17:35:34 +0300

    Usage lambda-expression with multiple params

---
+++ LambdaExpressionMultipleParams/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace LambdaExpressionMultipleParams
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SimpleMath simpleMath = new SimpleMath();
+            simpleMath.SetMathHandler((msg, result) => { Console.WriteLine("Message:{0} ,Result:{1} ", msg, result); });
+            simpleMath.Add(10, 10);
+            Console.ReadLine();
+        }
+        
+    }
+}
---
+++ LambdaExpressionMultipleParams/SimpleMath.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LambdaExpressionMultipleParams
+{
+    class SimpleMath
+    {
+        public delegate void MathMessage(string msg, int result);
+        private MathMessage mathMessage;
+        public void SetMathHandler(MathMessage message)
+        {
+            mathMessage = message;
+        }
+        public void Add(int a, int b)
+        {
+            mathMessage?.Invoke("Adding has complete!", a + b);
+        }
+    }
+}
