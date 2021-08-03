b9fdf1dc83892d79b5b28b41cd73fb03ed2033a6
SandERFills <salagaev98@mail.ru>
2021-08-03 11:22:21 +0300

    Studied special delegate,anonumous delegate and generic delegate

---
+++ CarEvent/Car.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarEvent
+{
+    public class CarEventArgs: EventArgs
+    {
+        public readonly string msg;
+        public CarEventArgs(string messsage)
+        {
+            msg = messsage;
+        }
+    }
+    class Car
+    {
+        /*
+            1. Определить новый тип делегата, который будет использоваться для отправки уведомлений вызывающему коду.
+            2. Объявить переменную-член этого типа делегата в классе Саг.
+            3. Создать в классе Саг вспомогательную функцию, которая позволяет вызывающему коду указывать метод для обратного вызова.
+            4. Реализовать метод Accelerate () для обращения к списку вызовов делегата в подходящих обстоятельствах.
+        Это нужно реализовать для создания мета уведомлений о состоянии мотора
+        */
+        public int CurrSpeed { get; set; }
+        public int MaxSpeed { get; set; }
+        public string CarName { get; set; }
+        private bool CarIsDead=false;
+        public Car() { }
+        public Car(string petname, int carspeed, int maxspeed)
+        {
+            CurrSpeed = carspeed;
+            CarName = petname;
+            MaxSpeed = maxspeed;
+        }
+        //1.определить тип делегата
+        public delegate void CarEngineHandler(object sender,CarEventArgs e);
+        public event EventHandler<CarEventArgs> Exploded;
+        public event EventHandler<CarEventArgs> AboutToBlow;
+        public void Accelerate(int delta)
+        {
+            if (CarIsDead)
+            {
+                
+                    Exploded?.Invoke(this,new CarEventArgs("Sorry.but your car is dead!"));
+            }
+            else
+            {
+                CurrSpeed += delta;
+                if (10==MaxSpeed-CurrSpeed)
+                {
+                    AboutToBlow?.Invoke(this, new CarEventArgs("Wow.slow down,your car is almost in heaven"));
+                }
+                if (CurrSpeed>=MaxSpeed)
+                {
+                    CarIsDead = true;
+                }
+                else
+                {
+                    Console.WriteLine("Current speed = {0}",CurrSpeed);
+                }
+            }
+        }
+    }
+}
---
+++ CarEvent/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace CarEvent
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Delegate lessons
+            //Car car1 = new Car("Sheggy", 20, 100);
+            //EventHandler<CarEventArgs> carEngineHandler = new EventHandler<CarEventArgs>(CarEventMessage);
+            //car1.AboutToBlow += carEngineHandler;
+            //Program program = new Program();
+            //car1.Exploded += program.CarDeadMessage;
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    car1.Accelerate(10);
+            //}
+            #endregion
+            #region Anonymous Methods
+            Console.WriteLine("****** Anonymous Methods *****");
+            Car car = new Car("Louge", 10, 120);
+            int aboutToBlowCounter=0;
+            car.AboutToBlow += delegate//Объявление анонимного метода
+            {
+                aboutToBlowCounter++;
+                Console.WriteLine("Eak,you're going too fast");
+            };
+            car.AboutToBlow += delegate (object sender, CarEventArgs e) {
+                if (sender is Car c)
+                {
+                    Console.WriteLine("{0} is message: {1}", c.CarName,e.msg);
+                    aboutToBlowCounter++;
+                }
+                
+            };
+            car.Exploded += delegate (object sender, CarEventArgs e) 
+            {
+                Console.WriteLine(e.msg); aboutToBlowCounter++;
+            };
+            for (int i = 0; i < 10; i++)
+            {
+                car.Accelerate(20);
+                Console.WriteLine("aboutToBlowCounter ={0}", aboutToBlowCounter) ;
+            }
+            #endregion
+
+        }
+
+
+        static void CarEventMessage(object sender,CarEventArgs msg)
+        {
+            if (sender is Car c)
+            {
+                Console.WriteLine("__________________");
+                Console.WriteLine("Car {1} is message:{0}\n", msg.msg, c.CarName) ;
+                Console.WriteLine("__________________");
+            }
+            
+        }
+        public void CarDeadMessage(object sender,CarEventArgs msg) {
+            if (sender is Car c)
+            {
+                Console.WriteLine("OOOoh {1},{0}", msg.msg,c.CarName);
+            }
+            
+        }
+    }
+}
