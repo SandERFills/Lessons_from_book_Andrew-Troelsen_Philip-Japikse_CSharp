6d06b05b96a009f1e37c89ae654ae8a2613cef7b
Vladislav Babich <salagaev98@mail.ru>
2021-08-08 19:01:08 +0300

    Beginning operator overload

---
+++ ManualDataTable/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Data;
+
+namespace ManualDataTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            MultiIndexerWithDataTable();
+        }
+        static void MultiIndexerWithDataTable(){
+            DataTable myTable=new DataTable();
+            myTable.Columns.Add(new DataColumn("FirstName"));
+            myTable.Columns.Add(new DataColumn("LastName"));
+            myTable.Columns.Add(new DataColumn("Age"));
+            //Добавить строку в таблицу
+            myTable.Rows.Add("Mel","Appleby",60);
+            System.Console.WriteLine("First name: {0}",myTable.Rows[0][0]);
+            System.Console.WriteLine("Last name: {0}",myTable.Rows[0][1]);
+            System.Console.WriteLine("Age: {0}",myTable.Rows[0][2]);
+        }
+    }
+}
---
+++ OverloadedOps/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace OverloadedOps
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Fun with operator override! *******");
+            Point p1=new Point(100,20);
+            Point p2=new Point(240,30);
+            Point p3 =p1-p2;
+            Point p4=p2+p1;
+            System.Console.WriteLine("{0}",p1);
+            System.Console.WriteLine("{0}",p2);
+            System.Console.WriteLine("{0}",p3);
+            System.Console.WriteLine("{0}",p4);
+        }
+    }
+}
