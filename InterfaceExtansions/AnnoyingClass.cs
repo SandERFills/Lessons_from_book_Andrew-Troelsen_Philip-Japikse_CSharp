a3b5e32f12ffb470d596eee62bf665dbc0f0d7ef
SandERFills <salagaev98@mail.ru>
2021-08-14 15:24:17 +0300

    lesson about extensions of interface

---
+++ InterfaceExtansions/AnnoyingClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceExtansions
+{
+    static class AnnoyingClass
+    {
+        public static void PrintDataAndBeep(this System.Collections.IEnumerable iterator)
+        {
+            foreach (var item in iterator)
+            {
+                Console.WriteLine(item);
+                Console.Beep();
+            }
+        }
+    }
+}
---
+++ InterfaceExtansions/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace InterfaceExtansions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("**** Extending Interface Compatible types!");
+
+            string[] stringArr = { "Воу", "Эта", "Сортировка", "Раздражает", "но", "весело", "странно" };
+            stringArr.PrintDataAndBeep();
+            List<int> intList = new List<int>{12, 424, 322, 44};
+            intList.PrintDataAndBeep();
+            Console.ReadLine();
+        }
+    }
+}
