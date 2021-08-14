85832a47ceaa38e905b3433a0098963ebd49480d
SandERFills <salagaev98@mail.ru>
2021-08-14 15:23:36 +0300

    studied how to realese extensions methods

---
+++ ExtensionMethods/MyExtension.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyExtensionMethods
+{
+    static class MyExtension
+    {
+      public static void DisplayDefiningAssambly(this object obj)
+        {
+            Console.WriteLine("{0} lives here: {1}", obj.GetType().Name, Assembly.GetAssembly(obj.GetType()).GetName().Name);
+
+        } 
+        public static int ReverseDigits(this int myInt)
+        {
+            char[] digits = myInt.ToString().ToCharArray();
+            Array.Reverse(digits);
+            string newDigits = new string(digits);
+            return int.Parse(newDigits);
+        }
+    }
+}
---
+++ ExtensionMethods/Program.cs
@@ -0,0 +1,19 @@
+﻿using MyExtensionMethods;
+using System;
+
+namespace ExtensionMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*******Fun With Extensions!********");
+            int MyInt = 123456789;
+            MyInt.DisplayDefiningAssambly();
+            Console.WriteLine(MyInt.ReverseDigits());
+            System.Data.DataSet dataSet = new System.Data.DataSet();
+            dataSet.DisplayDefiningAssambly();
+            
+        }
+    }
+}
