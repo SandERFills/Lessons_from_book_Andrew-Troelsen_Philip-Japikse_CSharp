85d382a61c9662d95192473f1c53b61a60ff6dfc
SandERFills <salagaev98@mail.ru>
2021-08-15 15:02:54 +0300

    Return LINQ as value

---
+++ LinqRetValues/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LinqRetValues
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** LINQ return values");
+            IEnumerable<string> subColorArr = GetStringSubset();
+            foreach (var c in subColorArr)
+            {
+                Console.WriteLine(c);
+            }
+        }
+        static IEnumerable<string> GetStringSubset()
+        {
+            string[] colors = { "Dark Red", "Blue", "Green", "Light Red", "Red" };
+            IEnumerable<string> subset = from i in colors where i.Contains("Red") select i;
+            return subset;
+        }
+        static IEnumerable<string> GetStringSubsetArray()
+        {
+            string[] colors = { "Dark Red", "Blue", "Green", "Light Red", "Red" };
+            var subset = from i in colors where i.Contains("Red") select i;
+            return subset.ToArray<string>();
+        }
+    }
+}
---
+++ ListOverCollections/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ListOverCollections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            List<Car> myCars = new List<Car>()
+            {
+                new Car { PetName = "Henry", Color = "Silver ", Speed = 100, Make = "BMW" },
+                new Car { PetName = "Daisy", Color = "Tan", Speed = 90, Make = "BMW" },
+                new Car { PetName = "Mary", Color = " Black", Speed = 55, Make = "VW" },
+                new Car { PetName = "Clunker ", Color = "Rust ", Speed = 5, Make = "Yugo" },
+                new Car { PetName = "Melvin", Color = "White ", Speed = 43, Make = "Ford" }
+            };
+            GetFastCars(myCars);
+        }
+        static void GetFastCars(List<Car> cars)
+        {
+            var fastCars = from c in cars where c.Speed > 55 select c;
+            foreach (var item in fastCars)
+            {
+                Console.WriteLine("Fasters car is {0}",item.PetName);
+            }
+        }
+    }
+}
