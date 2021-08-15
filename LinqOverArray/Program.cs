3dff302f82baafbfc0c4d95015f0b3699712c8a3
SandERFills <salagaev98@mail.ru>
2021-08-15 12:47:39 +0300

    LINQ query example

---
+++ LinqOverArray/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LinqOverArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("**** LINQ begging!");
+            QueryOverString();
+        }
+        static void QueryOverString()
+        {
+            string[] currentVidoGame = { "Morrowind", "Uncharted 2", "Fallout 2", "Command & Conquared", "System Shock" };
+            IEnumerable<string> subset = from g in currentVidoGame where g.Contains(" ") orderby g select g;
+            foreach (var s in subset)
+            {
+                Console.WriteLine("Item: {0}",s);
+            }
+        }
+    }
+}
