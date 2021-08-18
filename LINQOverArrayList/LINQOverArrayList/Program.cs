a21025b66fab4626701e3181e4025fa639102f15
SandERFills <salagaev98@mail.ru>
2021-08-18 18:21:41 +0300

    Keep study LINQ extension methods

--- LINQOverArrayList/LINQOverArrayList/Program.cs
+++ LINQOverArrayList/LINQOverArrayList/Program.cs
@@ -1,15 +1,16 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Collections;
 using System.Linq;
 
-namespace ListOverCollections
+namespace LINQOverArrayList
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            List<Car> myCars = new List<Car>()
+            Console.WriteLine("***** LINQ over ArrayList");
+            //Необобщённая коллекция не реализует интерфейс IEnumerable<T> для работы с LINQ
+            ArrayList myCars = new ArrayList()
             {
                 new Car { PetName = "Henry", Color = "Silver ", Speed = 100, Make = "BMW" },
                 new Car { PetName = "Daisy", Color = "Tan", Speed = 90, Make = "BMW" },
@@ -17,15 +18,14 @@ namespace ListOverCollections
                 new Car { PetName = "Clunker ", Color = "Rust ", Speed = 5, Make = "Yugo" },
                 new Car { PetName = "Melvin", Color = "White ", Speed = 43, Make = "Ford" }
             };
-            GetFastCars(myCars);
-        }
-        static void GetFastCars(List<Car> cars)
-        {
-            var fastCars = from c in cars where c.Speed > 55 && c.Make=="BMW" select c;
-            foreach (var item in fastCars)
+            //Трансформировать ArrayList в  IEnumerable<T>
+            var MyCarEnum = myCars.OfType<Car>();
+            var fastCar = from c in MyCarEnum where c.Speed > 55 select c;
+            foreach (var car in MyCarEnum)
             {
-                Console.WriteLine("Fasters car is {0}",item.PetName);
+                Console.WriteLine("Faster car:{0}",car.PetName);
             }
+
         }
     }
 }
