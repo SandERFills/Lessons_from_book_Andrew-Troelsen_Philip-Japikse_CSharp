4c521aadf6d44ef29c094d05e359c10a0434dc9a
SandERFills <salagaev98@mail.ru>
2021-08-22 12:54:59 +0300

    Realize of lazy<T> example

---
+++ LazyObjectInstantaition/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace LazyObjectInstantaition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("**** Fun  with Lazy Instantiation");
+            MediaPlayer mediaPlayer = new MediaPlayer();
+            mediaPlayer.Play();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ LazyObjectInstantaition/Song.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LazyObjectInstantaition
+{
+    class Song
+    {
+        public string Artist { get; set; }
+        public string TrackName { get; set; }
+        public double TrackLenght { get; set; }
+
+    }
+    class AllTracks
+    {
+        private Song[] allsongs = new Song[10000];
+        public AllTracks()
+        {
+            Console.WriteLine("Filling up the songs");
+        }
+    }
+    class MediaPlayer
+    {
+        public void Play() { Console.WriteLine("Music play"); }
+        public void Stop() { Console.WriteLine("Music stop"); }
+        public void Pause() { Console.WriteLine("Music pause"); }
+        private Lazy<AllTracks> allTracks = new Lazy<AllTracks>(()=> {
+            Console.WriteLine("All tacks add");
+            return new AllTracks();
+        });
+        public AllTracks GetAllTracks()
+        {
+            return allTracks.Value;
+        }
+    }
+}
